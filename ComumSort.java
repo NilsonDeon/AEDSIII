4b201e07c02888cfe71c03cd9c01fbe3444d4c76
Gabriel Vargas <105235647+gabrielvargas00@users.noreply.github.com>
2023-03-01 18:11:36 -0300

    Criar arquivos temporários em blocos de 1500

--- ComumSort.java
+++ ComumSort.java
@@ -6,10 +6,15 @@ public class ComumSort{
     private static final String registroDB = "Registro.db";
     private static final int REGISTROS = 1500;
     private static final int CAMINHOS = 4;
-    private RandomAccessFile arqTemp = null;
-    Musica[] musicas = new Musica[REGISTROS];
 
-    public ComumSort(){
+    public ComumSort(){}
+
+    public void distribuicao() throws IOException {
+
+        RandomAccessFile arqTemp = null;
+        RandomAccessFile dbFile = null;
+
+        // Criar os 4 arquivos temporarios
         for (int i = 0; i < CAMINHOS; i++) {
             try {
                 arqTemp = new RandomAccessFile("arqTemp"+ i +".db", "rw");
@@ -20,51 +25,51 @@ public class ComumSort{
                                    "arquivo \"" + registroDB + "\"\n");
             }
         }
-    }
-
-    public void distribuicao() throws IOException {
-
-        RandomAccessFile dbFile = null;
 
         try {
             dbFile = new RandomAccessFile (registroDB, "rw");
-            int tamanho = dbFile.readInt();
 
-            int i = 0;
-            while (i < tamanho) {
+            dbFile.seek(0);
+            int ultimoId = dbFile.readInt();
+            long posicaoAtual = dbFile.getFilePointer();
+
+            Musica[] musicas;
+
+            while (dbFile.length() != posicaoAtual) {
                 for(int k = 0; k < 4; k++){
                     int posArray = 0;
-                    while (i < tamanho && posArray < 1500) {
+                    musicas = new Musica[REGISTROS];
+                    while (dbFile.length() != posicaoAtual && posArray < 1500) {
                         boolean lapide = dbFile.readBoolean();
                         int tamRegistro = dbFile.readInt();
-                       if( lapide == true){
+                        if (lapide == false){
                             byte[] registro = new byte[tamRegistro];
                             dbFile.read(registro);
                             musicas[posArray] = new Musica();
                             musicas[posArray].fromByteArray(registro);
-                            //System.out.println(musicas[0]);
                             posArray++;
                         } else {
-                            long posicaoAtual = dbFile.getFilePointer();
+                            posicaoAtual = dbFile.getFilePointer();
                             long proximaPosicao = posicaoAtual + (long)tamRegistro;
                             dbFile.seek(proximaPosicao);
-                            i++;
                         }
-                        QuickSort quick = new QuickSort(REGISTROS);
-                        quick.quicksort(musicas, 0, posArray);
-                        for (Musica musica : musicas) {
-                            byte[] bytes = musica.toByteArray();
-                            RandomAccessFile temp = null;
-                            try{
-                                temp = new RandomAccessFile ("arqTemp" + k + ".db", "rw");
-                                temp.write(bytes);
+                        posicaoAtual = dbFile.getFilePointer();
+                    }
+                    //QuickSort quick = new QuickSort(posArray);
+                    //quick.quicksort(musicas);
+                    for (int i = 0; i < posArray; i++) {
+                        byte[] bytes = musicas[i].toByteArray();
+                        RandomAccessFile temp = null;
+                        try{
+                            temp = new RandomAccessFile ("arqTemp" + k + ".db", "rw");
+                            temp.seek(temp.length());
+                            temp.write(bytes);
 
-                            }catch(Exception e) {
-                                System.out.println("\nERRO: Ocorreu um erro na leitura do " +
-                                "arquivo \"" + "arqTemp" + k + " " + e +"\"\n");
-                            }finally{
-                                temp.close();
-                            }
+                        }catch(Exception e) {
+                            System.out.println("\nERRO: Ocorreu um erro na leitura do " +
+                            "arquivo \"" + "arqTemp\"" + k + " -> " + e +"\n");
+                        }finally{
+                            temp.close();
                         }
                     }
                 }
@@ -72,7 +77,7 @@ public class ComumSort{
 
        } catch (Exception e) {
             System.out.println("\nERRO: Ocorreu um erro na leitura do " +
-            "arquivo \"" + registroDB + " " + e +"\"\n");
+            "arquivo \"" + registroDB + "\" -> " + e +"\n");
        }finally{
             if (dbFile != null) {
                 dbFile.close();
