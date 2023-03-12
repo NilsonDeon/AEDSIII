f9d41e9fb8862c5ee6fde4c0fd821e53e6390a1c
Gabriel Vargas <105235647+gabrielvargas00@users.noreply.github.com>
2023-03-12 01:28:25 -0300

    Update parametrizacao ordenacoes

--- src/main/sort/OrdenacaoExterna.java
+++ src/main/sort/OrdenacaoExterna.java
@@ -11,13 +11,10 @@ public class OrdenacaoExterna {
     public void ordenarArquivo () throws Exception {
 
         IO io = new IO();
-
-        ComumSort sort1 = new ComumSort();
-        TamanhoVariavelSort sort2 = new TamanhoVariavelSort();
-        SelecaoPorSubstituicaoSort sort3 = new SelecaoPorSubstituicaoSort();
-
-        int atributo = -1;
-        int opcao = -1;
+        int opcao = 0;
+        int atributo = 0;
+        int numCaminhos = 0;
+        int numRegistros = 0;
 
         String menu1 = "\n+------------------------------------------+" +
                        "\n|    Escolha o atributo para ordenacao:    |" +
@@ -27,7 +24,16 @@ public class OrdenacaoExterna {
                        "\n| 3 - Data de lancamento                   |" +
                        "\n+------------------------------------------+";    
 
-       do {
+        String menu2 = "\n+------------------------------------------+" +
+                       "\n|        MENU INTECALACAO BALANCEADA       |" +
+                       "\n|------------------------------------------|" +
+                       "\n| 1 - Comum                                |" +
+                       "\n| 2 - Blocos de Tamanho Variavel           |" +
+                       "\n| 3 - Selecao por Substituicao             |" +
+                       "\n| 4 - Voltar                               |" +
+                       "\n+------------------------------------------+";          
+
+        do {
            try {
                System.out.println(menu1);
                atributo = io.readInt("\nDigite o atributo desejado: ");
@@ -42,28 +48,42 @@ public class OrdenacaoExterna {
            }
        } while (atributo < 1 || atributo > 3);
 
+       try {
+            // Caso valor lido for invalido, construtor trata execao
+            numCaminhos  = io.readInt("\nDigite o numero de caminhos : ");
+            numRegistros = io.readInt("\nDigite o numero de registros: ");
 
-        String menu2 = "\n+------------------------------------------+" +
-                       "\n|        MENU INTECALACAO BALANCEADA       |" +
-                       "\n|------------------------------------------|" +
-                       "\n| 1 - Comum                                |" +
-                       "\n| 2 - Blocos de Tamanho Variavel           |" +
-                       "\n| 3 - Selecao por Substituicao             |" +
-                       "\n| 4 - Voltar                               |" +
-                       "\n+------------------------------------------+";        
+            if (numCaminhos < 2 || numRegistros < 1) {
+                System.out.println("\nERRO: valores invalidos!");
+                System.out.println("Redefinido para 4 caminhos e 1500 registros!");
+            }
+
+        } catch (InputMismatchException e) {
+            io.readLine();
+        }
 
        do {
            try {
-               System.out.println(menu2);
-               opcao = io.readInt("\nDigite a ordenacao desejada: ");
 
-               switch (opcao) {
-                   case 1 : sort1.ordenar(atributo); break;
-                   case 2 : sort2.ordenar(atributo); break;
-                   case 3 : sort3.ordenar(atributo); break;
+                System.out.println(menu2);
+                opcao = io.readInt("\nDigite a ordenacao desejada: ");
+
+                switch (opcao) {
+                   case 1 :
+                        ComumSort sort1 = new ComumSort(numRegistros, numCaminhos);
+                        sort1.ordenar(atributo); 
+                        break;
+                   case 2 :
+                        TamanhoVariavelSort sort2 = new TamanhoVariavelSort(numRegistros, numCaminhos);
+                        sort2.ordenar(atributo); 
+                        break;                   
+                   case 3 :
+                        SelecaoPorSubstituicaoSort sort3 = new SelecaoPorSubstituicaoSort(numRegistros, numCaminhos);
+                        sort3.ordenar(atributo); 
+                        break;
                    case 4 : break;
                    default: System.out.println("\nERRO: Por favor, digite uma opcao valida de 1 a 4."); break;
-               }
+                }
            } catch (InputMismatchException e) {
                System.out.println("\nERRO: Por favor, digite uma opcao valida de 1 a 4.");
                io.readLine();
